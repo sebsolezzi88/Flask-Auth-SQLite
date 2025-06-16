65ec9e0f1770cfaaa80fd51a313362528a7e78d2
sebsolezzi88 <ssolezzi1@gmail.com>
2025-06-15 21:58:16 -0300

    Creando Pagina de Login

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def index():
 def registro():
     return render_template('registro.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
